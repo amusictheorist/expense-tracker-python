3ca47f48da4b1d3f06e66ca0c972fd6f7f6407a7
amusictheorist <a_millaresthomson@outlook.com>
2025-01-21 09:37:12 -0500

    Create save_data method

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ def load_data():
       return json.load(file)
   except FileNotFoundError:
     return {'expenses': [], 'budgets': {}}
+  
+def save_data(data):
+  with open('data.json', 'w') as file:
+    json.dump(data, file, indent=4)
+
